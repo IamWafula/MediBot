a515346320be62514608335a59a8bb55608ee911
Ian Wafula <46840113+IamWafula@users.noreply.github.com>
2023-02-17 22:56:05 -0800

    Components for Wisdom
    Created structure for Wisdom (cause he a lil bitch)

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>inshallah</Text>
       <StatusBar style="auto" />
+      <Navbar />
     </View>
   );
 }
